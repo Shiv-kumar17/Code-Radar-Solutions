db9ee1b0120d93888c2391f421146f6cd4c4084a
Shiv-kumar17 <shivmk3705@gmail.com>
2025-02-06 18:22:30 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -3,14 +3,15 @@
 int main() {
     int num1, num2;
 
-    // Reading two space-separated integers
+    // Prompting the user to enter two space-separated integers
+    printf("Enter two integers: ");
     scanf("%d %d", &num1, &num2);
 
-    // Check if both numbers are greater than zero
+    // Checking if both numbers are greater than zero
     if (num1 > 0 && num2 > 0) {
         printf("True\n");
     } else {
-        printf("True\n");
+        printf("False\n");
     }
 
     return 0;
