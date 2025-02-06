1f6464a095f5eee9b60954afbb071c4a947a1789
Shiv-kumar17 <shivmk3705@gmail.com>
2025-02-06 18:20:29 +0530

    Solution to Logical OR Operation

---
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Reading two space-separated integers
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+    printf("True")
+
+    return 0;
+}
